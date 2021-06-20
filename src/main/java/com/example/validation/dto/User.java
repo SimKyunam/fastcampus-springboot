b358dc6ac7c00e53eb8f845f1c8c63ace885ad32
kyunam Sim <ppp9026@naver.com>
2021-06-20 19:03:51 +0900

    validation 메소드 custom 어노테이션 custom

--- src/main/java/com/example/validation/dto/User.java
+++ src/main/java/com/example/validation/dto/User.java
@@ -1,7 +1,6 @@
 package com.example.validation.dto;
 
-import com.example.validation.annotation.YearMonth;
-
+import javax.validation.Valid;
 import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -15,15 +14,10 @@ public class User {
     @Max(value = 90)
     private int age;
 
-    private String email;
-
-    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "핸드폰 번호 일치하지 않습니다. 01x-xxx(x)-xxxx")
-    private String phoneNumber;
-
-    @YearMonth
-    private String reqYearMonth; //yyyyMM
-
+    @Valid
     private List<Car> cars;
+
+//    private String reqYearMonth;
 
     public String getName() {
         return name;
@@ -41,38 +35,39 @@ public class User {
         this.age = age;
     }
 
-    public String getEmail() {
-        return email;
+    public List<Car> getCars() {
+        return cars;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
     }
 
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getReqYearMonth() {
-        return reqYearMonth;
-    }
-
-    public void setReqYearMonth(String reqYearMonth) {
-        this.reqYearMonth = reqYearMonth;
-    }
+//    public String getReqYearMonth() {
+//        return reqYearMonth;
+//    }
+//
+//    public void setReqYearMonth(String reqYearMonth) {
+//        this.reqYearMonth = reqYearMonth;
+//    }
+//
+//    @AssertTrue(message = "yyyyMM 의 형식에 맞지 않습니다.") //메소드에 적용할 때
+//    public boolean isReqYearMonthValidation(){
+//        try {
+//            LocalDate localDate = LocalDate.parse(getReqYearMonth()+"01", DateTimeFormatter.ofPattern("yyyyMMdd"));
+//        }catch (Exception e){
+//            return false;
+//        }
+//
+//        return true;
+//    }
 
     @Override
     public String toString() {
         return "User{" +
                 "name='" + name + '\'' +
                 ", age=" + age +
-                ", email='" + email + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", reqYearMonth='" + reqYearMonth + '\'' +
+                ", cars=" + cars +
                 '}';
     }
 }
