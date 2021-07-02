1f668b0b72be85488b82371f38808550b4a28fb8
DESKTOP-7FFOCP5\SKN <ppp9026@naver.com>
2021-07-02 15:20:42 +0900

    naver api 연동

--- src/main/java/com/example/server/controller/ServerApiController.java
+++ src/main/java/com/example/server/controller/ServerApiController.java
@@ -3,13 +3,55 @@ package com.example.server.controller;
 import com.example.server.dto.Req;
 import com.example.server.dto.User;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpEntity;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/server")
 public class ServerApiController {
+
+    //https://openapi.naver.com/v1/search/local.json
+    // ?query=%EC%A3%BC%EC%8B%9D
+    // &display=10
+    // &start=1
+    // &sort=random
+    @GetMapping("/naver")
+    public String naver(){
+
+        String query = "중국집";
+
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://openapi.naver.com")
+                .path("/v1/search/local.json")
+                .queryParam("query", query)
+                .queryParam("display", 10)
+                .queryParam("start", 1)
+                .queryParam("sort", "random")
+                .encode()
+                .build()
+                .toUri();
+
+        log.info("uri : {}", uri);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        RequestEntity<Void> req = RequestEntity
+                .get(uri)
+                .header("X-Naver-Client-Id", "G9P3uJxUcQQFXQRi4QcL")
+                .header("X-Naver-Client-Secret", "3l0JhUci6o")
+                .build();
+
+        ResponseEntity<String> result = restTemplate.exchange(req, String.class);
+        return result.getBody();
+    }
 
     @GetMapping("")
     public User hello(@RequestParam String name, @RequestParam int age){
