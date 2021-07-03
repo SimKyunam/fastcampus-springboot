c676a3906f33b4f7dc447785d64f4ae4ab4cc286
kyunam Sim <ppp9026@naver.com>
2021-07-03 16:12:31 +0900

    junit, mockito 사용

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,16 @@
+import java.net.DatagramPacket;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello JUnit");
 
-        Calculator calculator = new Calculator(new KrwCalculator());
+        MarketApi marketApi = new MarketApi();
+        DollarCalculator dollarCalculator = new DollarCalculator(marketApi);
+        dollarCalculator.init();
+
+//        Calculator calculator = new Calculator(new KrwCalculator());
+        Calculator calculator = new Calculator(dollarCalculator);
         System.out.println(calculator.sum(10, 10));
     }
 }
