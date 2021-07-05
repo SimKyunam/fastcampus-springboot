df3ce48e68f3c0c6678e621caeadce160fc3b9b3
kyunam Sim <ppp9026@naver.com>
2021-07-06 07:09:06 +0900

    Initial commit

---
+++ src/main/java/com/example/restaurant/wishlist/repository/WishListRepository.java
@@ -0,0 +1,6 @@
+package com.example.restaurant.wishlist.repository;
+
+import com.example.restaurant.db.MemoryDbRepositoryAbstract;
+
+public class WishListRepository extends MemoryDbRepositoryAbstract {
+}
