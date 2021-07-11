65402e2194348a896ab6d42de52e2e9e6d4d824f
kyunam Sim <ppp9026@naver.com>
2021-07-11 12:32:54 +0900

    swagger 설정 추가

--- src/main/java/com/example/swagger/dto/UserReq.java
+++ src/main/java/com/example/swagger/dto/UserReq.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserReq {
 
-    @ApiModelProperty(value="사용자의 이름", example = "steve", required = true)
+    @ApiModelProperty(value="사용자의 이름", example = "홍길동", required = true)
     private String name;
 
     @ApiModelProperty(value="사용자의 나이", example = "10", required = true)
